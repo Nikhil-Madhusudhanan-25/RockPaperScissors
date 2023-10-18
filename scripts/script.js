cdf47cb89ef2d60e8995030bbea0959b12f6c1fa
nikhil-madhusudhanan-25 <nikhilmadhusudhanan20@gmail.com>
2023-10-18 16:21:30 +0530

    Add two functions, one to get i/p from user, one with the game logic

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,41 @@ function getComputerChoice(){
         return "paper";
     return "scissors";
 }
-function playRound(){
-    console.log(getComputerChoice());
-
+function getUserChoice(){
+    let userChoice= prompt("Enter your choice, Rock/Paper/Scissors");
+    userChoice= userChoice.toLowerCase();
+    if(userChoice=="rock"||userChoice=="paper"||userChoice=="scissors")
+        playRound(userChoice);
+    else
+        alert("Invalid input")
+}
+function playRound(userChoice){
+    let computerChoice= getComputerChoice();
+    if(computerChoice=="rock")
+    {
+        if(userChoice=="paper")
+            alert("Computer choice is "+computerChoice+", you win!");
+        else if(userChoice=="scissors")
+            alert("Computer choice is "+computerChoice+", Computer wins!");
+        else
+            alert("Computer choice is "+computerChoice+", its a draw!");
+    }
+    else if (computerChoice=="scissors")
+    {
+        if(userChoice=="paper")
+            alert("Computer choice is "+computerChoice+", Computer win!");
+        else if(userChoice=="scissors")
+            alert("Computer choice is "+computerChoice+", its a draw!");
+        else
+            alert("Computer choice is "+computerChoice+", you win!");
+    }
+    else
+    {
+        if(userChoice=="paper")
+            alert("Computer choice is "+computerChoice+", its a draw!");
+        else if(userChoice=="scissors")
+            alert("Computer choice is "+computerChoice+", you win!");
+        else
+            alert("Computer choice is "+computerChoice+", Computer wins!");
+    }
 }
